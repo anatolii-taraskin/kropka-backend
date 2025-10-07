89e1d3b23b9379389ea2005bc5ca23e8a178f670
Anatolii Taraskin <WolfgangSeven@gmail.com>
2025-10-07 07:40:03 +0400

    Fix studio info table name

--- database/migrations/2025_10_07_032144_create_studio_info_table.php
+++ database/migrations/2025_10_07_032144_create_studio_info_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('studio_info', function (Blueprint $table) {
+        Schema::create('studio_infos', function (Blueprint $table) {
             $table->id();
             $table->string('property')->unique();
             $table->text('value');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('studio_info');
+        Schema::dropIfExists('studio_infos');
     }
 };
