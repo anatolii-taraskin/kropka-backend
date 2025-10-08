d12aab572b1bca9857734c110780c9ec051220b2
Anatolii Taraskin <WolfgangSeven@gmail.com>
2025-10-08 19:13:04 +0400

    Add status flag and sort order to prices

---
+++ app/Models/Price.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'col1',
+        'col2',
+        'col3',
+        'is_active',
+        'sort',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,6 +32,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             StudioInfoSeeder::class,
             StudioRuleSeeder::class,
+            PriceSeeder::class,
         ]);
     }
 }
