53f8b0c6ee5e29ca1c7a5936de9c928340f5ef42
Anatolii Taraskin <anatoliy.taraskin@advcake.com>
2025-10-09 02:51:30 +0400

    redraw button color from blue to red

--- resources/views/admin/prices/index.blade.php
+++ resources/views/admin/prices/index.blade.php
@@ -149,7 +149,7 @@
         <div class="flex justify-start p-6">
             <a
                 href="{{ route('admin.prices.create') }}"
-                class="inline-flex items-center gap-2 rounded-md bg-blue-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white shadow-sm transition duration-150 ease-in-out hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                class="inline-flex items-center gap-2 rounded-md bg-red-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white shadow-sm transition"
             >
                 <svg class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd"
