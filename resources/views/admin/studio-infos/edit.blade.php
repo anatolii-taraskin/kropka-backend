156d9f04bbb43dd5c8ce17db791bc150eec1c082
Anatolii Taraskin <anatoliy.taraskin@advcake.com>
2025-10-08 08:17:40 +0400

    Bringing page styles in the admin panel to a unified style

--- resources/views/admin/studio-infos/edit.blade.php
+++ resources/views/admin/studio-infos/edit.blade.php
@@ -5,21 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-2 pb-3 lg:px-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl pb-3">
                     <section>
-                        <header>
-                            <h2 class="text-lg font-medium text-gray-900">
-                                {{ __('admin.studio_infos.title') }}
-                            </h2>
-
-                            <p class="mt-1 text-sm text-gray-600">
-                                {{ __('admin.studio_infos.description') }}
-                            </p>
-                        </header>
-
                         @if (session('status') === 'studio-infos-updated')
                             <x-alert-success class="mt-4">
                                 {{ __('admin.studio_infos.success') }}
