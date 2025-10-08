aa19e84632e3b251e475b43fff8b5cd7fc14f380
Anatolii Taraskin <WolfgangSeven@gmail.com>
2025-10-08 08:36:42 +0400

    Extend admin page component to wrap shared body layout

---
+++ resources/views/components/admin/page.blade.php
@@ -0,0 +1,27 @@
+@props([
+    'title',
+    'containerClass' => 'p-2 pb-3 lg:px-8 bg-white shadow sm:rounded-lg',
+    'contentClass' => 'max-w-xl pb-3',
+])
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $title }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div {{ $attributes->class($containerClass) }}>
+                @if ($contentClass)
+                    <div class="{{ $contentClass }}">
+                        {{ $slot }}
+                    </div>
+                @else
+                    {{ $slot }}
+                @endif
+            </div>
+        </div>
+    </div>
+</x-app-layout>
