6be3ee75370791c6093cdda8fc55e6e2593d96f5
Anatolii Taraskin <WolfgangSeven@gmail.com>
2025-10-09 04:11:33 +0400

    Restore inline button styles with consistent hover and active states

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn-primary']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest text-white transition ease-in-out duration-150 bg-green-600 hover:bg-green-500 focus:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2']) }}>
     {{ $slot }}
 </button>
